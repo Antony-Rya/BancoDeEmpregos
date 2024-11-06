af66b83bba09846b070233ef8961bee9dcceb716
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2024-11-06 17:22:49 +0000

    Criação

---
+++ Banco_de_Empregos/Site/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Oferta(models.Model){
+    empresa = models.Charfield(maxleng)
+    cacrgo
+    requisito
+    salario
+    experiencia
+    carga
+    beneficios
+    regime
+    vagas
+    detalhes
+}
